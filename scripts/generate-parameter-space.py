017a038be3836f88dcc4d5e976e4e8503ecb1083
Maximilian Konzack <maximilian.konzack@idiv.de>
2019-03-08 17:17:26 +0100

    Generate a smaller parameter space from util

--- scripts/generate-parameter-space.py
+++ scripts/generate-parameter-space.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import numpy as np
+import utils
 
 def parameter_combinations():
     for dim in np.linspace(start=10, stop=1000, num=100, dtype=int):
@@ -11,5 +12,5 @@ def parameter_combinations():
                     for bucket in np.linspace(start=2_000_000, stop=10_000_000, num=5, dtype=int):
                         yield dim, lr, wordNgrams, epoch, bucket
 
-df = pd.DataFrame(parameter_combinations(), columns=['dim', 'lr', 'wordNgrams', 'epoch', 'bucket'])
+df = pd.DataFrame(utils.parameter_combinations(), columns=['dim', 'lr', 'wordNgrams', 'epoch', 'bucket'])
 df.to_csv(snakemake.output[0], index=False)

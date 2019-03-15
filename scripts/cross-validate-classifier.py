130a44a2a63840e0c4c029711f3e64fbf30bc55f
Maximilian Konzack <maximilian.konzack@idiv.de>
2019-03-15 15:45:49 +0100

    Keep track of threads variable and allowing running this script without snakemake

--- scripts/cross-validate-classifier.py
+++ scripts/cross-validate-classifier.py
@@ -5,8 +5,7 @@ import fastText
 
 from utils import f1_score
 
-ELEMS = ['dim', 'lr', 'wordNgrams', 'epoch', 'bucket']
-
+THREADS = 1
 
 def cross_validate_model(model_parameters, cross_validation_sets):
     f1_scores = []
@@ -41,28 +40,37 @@ def eval_test_data(params_csv, train_data, test_data):
         f1_score = test_model(params, train_data, test_data)
         params_csv.at[params.Index, 'f1_test'] = f1_score
 
-params_csv = pd.read_csv(snakemake.input.params_csv)
-params_csv['f1_cross_validation'] = None
-params_csv['f1_test'] = None
 
-cv_sets = list(zip(snakemake.input.train_data, snakemake.input.valid_data))
-
-
-eval_model_parameters(params_csv, cv_sets)
-eval_test_data(params_csv, snakemake.input.train_file, snakemake.input.test_file)
-
-# f1_scores = []
-# for train_file, valid_file in cv_sets:
-#     classifier = fastText.train_supervised(train_file,
-#     dim=10, lr=1.5, wordNgrams=3, minCount=1, bucket=10_000_000,
-#     epoch=50, thread=4)
-
-#     _, precision, recall = classifier.test(valid_file)
-#     f1 = f1_score(precision, recall)
-#     f1_scores.append(f1)
+def main():
+    global THREADS
+    if 'snakemake' in globals():
+        parameters_file_name = snakemake.input.params_csv
+        cv_sets = list(zip(snakemake.input.train_data, snakemake.input.valid_data))
+        train_file_name = snakemake.input.train_file
+        test_file_name = snakemake.input.test_file
+        result_csv = snakemake.output.result
+        THREADS = snakemake.threads
+    else:
+        import sys
+        from pathlib import Path
+        _, parameters_file_name, cv_dir, result_csv, THREADS = sys.argv
+        cv_path = Path(cv_dir)
+        train_file_name = str(next(cv_path.glob('*.train')))
+        test_file_name = str(next(cv_path.glob('*.test')))
+        cv_train_data = sorted(map(lambda p: str(p), cv_path.glob('set_*/*.train')))
+        cv_valid_data = sorted(map(lambda p: str(p), cv_path.glob('set_*/*.train')))
+        cv_sets = list(zip(cv_train_data, cv_valid_data))
     
-# mean_f1 = sum(f1_scores) / snakemake.params.kfold
-# params_csv['f1_train_mean'] = mean_f1
-#print(mean_f1)
+    print("I am using {} threads".format(THREADS))
+    params_csv = pd.read_csv(parameters_file_name)
+    params_csv['f1_cross_validation'] = None
+    params_csv['f1_test'] = None
+    
+    eval_model_parameters(params_csv, cv_sets)
+    eval_test_data(params_csv, train_file_name, test_file_name)
 
-params_csv.to_csv(snakemake.output.result, index=False)
+    params_csv.to_csv(result_csv, index=False)
+
+
+if __name__ == "__main__":
+    main()

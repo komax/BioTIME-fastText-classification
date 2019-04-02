0c9c307fb5b6b03d1a01ccefdb12c2b0641f9148
Maximilian Konzack <maximilian.konzack@idiv.de>
2019-04-02 16:58:24 +0200

    New implementation using dataframes to keep track of scores on each cross validation set

--- scripts/cross-validate-classifier.py
+++ scripts/cross-validate-classifier.py
@@ -9,21 +9,14 @@ from utils import f1_score, ResultScore
 THREADS = 1
 
 
-def scores_pooled_from_labels(model, path_test_file, k=1, threshold=0.0):
+def scores_per_label(model, path_test_file, k=1, threshold=0.0):
     scores = model.test_label(path_test_file, k=k, threshold=threshold)
-    precisions = list()
-    recalls = list()
-    f1_scores = list()
 
-    for score in scores.values():
-        precisions.append(score['precision'])
-        recalls.append(score['recall'])
-        f1_scores.append(score['f1score'])
+    result = pd.DataFrame(columns=['label', 'precision', 'recall', 'f1score'])
 
-    pooled_precision = np.mean(precisions)
-    pooled_recall = np.mean(recalls)
-    pooled_f1 = np.mean(f1_scores)
-    return ResultScore(pooled_precision, pooled_recall, pooled_f1)
+    for label, score in scores.items():
+        result = result.append({'label':label, 'precision':score['precision'], 'recall':score['recall'], 'f1score':score['f1score']}, ignore_index=True)
+    return result
 
 
 def avg_result_score(result_score):
@@ -31,11 +24,29 @@ def avg_result_score(result_score):
         recall=np.mean(result_score.recall), f1score=np.mean(result_score.f1score))
     return result
 
+# FIXME remove this.
+def peak_to_peak_result_score(result_score):
+    ResultScore(precision=np.ptp(result_score.precision),
+        recall=np.ptp(result_score.recall),
+        f1score=np.ptp(result_score.f1score))
+
+
+def min_result_score(result_score):
+    return ResultScore(precision=np.min(result_score.precision),
+        recall=np.min(result_score.recall), f1score=np.min(result_score.f1score))
+
+
+def max_result_score(result_score):
+    return ResultScore(precision=np.max(result_score.precision),
+        recall=np.max(result_score.recall), f1score=np.max(result_score.f1score))
+
 
 def cross_validate_model(model_parameters, cross_validation_sets):
-    micro_results = ResultScore(precision=[], recall=[], f1score=[])
     macro_results = ResultScore(precision=[], recall=[], f1score=[])
-    for train_file, valid_file in cross_validation_sets:
+    
+    micro_result = pd.DataFrame(columns=['cross_valid', 'label', 'precision', 'recall', 'f1score'])
+
+    for cv_id, (train_file, valid_file) in enumerate(cross_validation_sets):
         # Train the classifier.
         classifier = fastText.train_supervised(train_file,
             dim=model_parameters.dim, lr=model_parameters.lr,
@@ -47,15 +58,18 @@ def cross_validate_model(model_parameters, cross_validation_sets):
         macro_results.precision.append(precision)
         macro_results.recall.append(recall)
         macro_results.f1score.append(f1_score(precision, recall))
-        # Test the model on micro level (each class).
-        result = scores_pooled_from_labels(classifier, valid_file)
-        micro_results.precision.append(result.precision)
-        micro_results.recall.append(result.recall)
-        micro_results.f1score.append(result.f1score)
+
+        # Validation per class.
+        score_for_cv_set = scores_per_label(classifier, valid_file)
+        number_entries = len(score_for_cv_set.index)
+        # Enter micro scores to results table across cross validation sets.
+        repeated_cv_name_df = pd.DataFrame(number_entries * [f"cv{cv_id}"], columns=["cross_valid"])
+        reshaped_scores = pd.concat([repeated_cv_name_df, score_for_cv_set], axis=1)
+        micro_result = micro_result.append(reshaped_scores)
 
     # Reduce step: Calculate the means from the lists.
     macro_result = avg_result_score(macro_results)
-    micro_result = avg_result_score(micro_results)
+
     results = {
         'macro': macro_result,
         'micro': micro_result
@@ -68,6 +82,7 @@ def test_model(model_parameters, train_data_file, test_data_file):
             dim=model_parameters.dim, lr=model_parameters.lr,
             wordNgrams=model_parameters.wordNgrams, minCount=1,
             bucket=model_parameters.bucket, epoch=model_parameters.epoch, thread=THREADS)
+    # TODO Store f1 from micro on this test set.
     _, precision, recall = classifier.test(test_data_file)
     return f1_score(precision, recall)
 
@@ -82,9 +97,13 @@ def eval_model_parameters(params_csv, cv_sets):
         params_csv.at[params.Index, 'recall_macro'] = results['macro'].recall
         params_csv.at[params.Index, 'f1_cross_validation_macro'] = results['macro'].f1score
 
-        params_csv.at[params.Index, 'precision_micro'] = results['micro'].precision
-        params_csv.at[params.Index, 'recall_micro'] = results['micro'].recall
-        params_csv.at[params.Index, 'f1_cross_validation_micro'] = results['micro'].f1score
+        params_csv.at[params.Index, 'precision_micro'] = results['micro'].precision.mean()
+        params_csv.at[params.Index, 'recall_micro'] = results['micro'].recall.mean()
+        params_csv.at[params.Index, 'f1_cross_validation_micro'] = results['micro'].f1score.mean()
+
+        params_csv.at[params.Index, 'precision_micro_ptp'] = results['micro'].precision.ptp()
+        params_csv.at[params.Index, 'recall_micro_ptp'] = results['micro'].recall.ptp()
+        params_csv.at[params.Index, 'f1_cross_validation_micro_ptp'] = results['micro'].f1score.ptp()
 
 
 def eval_test_data(params_csv, train_data, test_data):
@@ -110,7 +129,7 @@ def main():
         train_file_name = str(next(cv_path.glob('*.train')))
         test_file_name = str(next(cv_path.glob('*.test')))
         cv_train_data = sorted(map(lambda p: str(p), cv_path.glob('set_*/*.train')))
-        cv_valid_data = sorted(map(lambda p: str(p), cv_path.glob('set_*/*.train')))
+        cv_valid_data = sorted(map(lambda p: str(p), cv_path.glob('set_*/*.valid')))
         cv_sets = list(zip(cv_train_data, cv_valid_data))
     
     # Ensure it's an int.
@@ -125,6 +144,10 @@ def main():
     params_csv['precision_micro'] = None
     params_csv['recall_micro'] = None
     params_csv['f1_cross_validation_micro'] = None
+
+    params_csv['precision_micro_ptp'] = None
+    params_csv['recall_micro_ptp'] = None
+    params_csv['f1_cross_validation_micro_ptp'] = None
 
     params_csv['f1_test'] = None
     

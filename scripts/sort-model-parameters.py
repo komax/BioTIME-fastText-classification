c734e3fd0d0ce6f4767a955a4467453c79ac6354
Maximilian Konzack <maximilian.konzack@idiv.de>
2019-04-03 10:46:36 +0200

    Write the sorted csv as output

--- scripts/sort-model-parameters.py
+++ scripts/sort-model-parameters.py
@@ -8,4 +8,4 @@ parameter_scores = pd.read_csv(snakemake.input.params_scores)
 sorted_scores = parameter_scores.sort_values(by=['f1_cross_validation_micro','f1_cross_validation_micro_ptp'],
 ascending=[False,True] )
 
-parameter_scores.to_csv(snakemake.output.scores_sorted, index=False)
+sorted_scores.to_csv(snakemake.output.scores_sorted, index=False)
